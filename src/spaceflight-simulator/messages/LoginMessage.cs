1fc17b0a0e78a1fb65a1c895418e2abe333348b5
Christopher Rowe <chris.rowe19@outlook.com>
2023-04-16 21:10:13 +0100

    Adds a Unity client project.
    Also starts work on messages.

--- src/spaceflight-simulator/messages/LoginMessage.cs
+++ src/spaceflight-simulator/messages/LoginMessage.cs
@@ -9,16 +9,16 @@ using spaceflight_simulator.network_objects.datatypes;
 
 namespace spaceflight_simulator.messages
 {
-    public sealed class TestServerMessage : network_objects.Message
+    public sealed class LoginMessage : network_objects.Message
     {
-        public TestServerMessage() : base(1, 3) { }
+        public LoginMessage() : base(2, 3) { }
 
         protected override bool ValidateDataValues(ref Type[] types, ref NetworkedDataObject[] input_data)
         {
             return types[0] == typeof(network_objects.datatypes.String) && types[1] == typeof(network_objects.datatypes.Vector3) && types[2] == typeof(network_objects.datatypes.Vector3);
         }
 
-        public TestServerMessage(network_objects.datatypes.String name, network_objects.datatypes.Vector3 position, network_objects.datatypes.Vector3 velocity) : this()
+        public LoginMessage(network_objects.datatypes.String name, network_objects.datatypes.Vector3 position, network_objects.datatypes.Vector3 velocity) : this()
         {
             data = new NetworkedDataObject[] { name, position, velocity };
         }
--- src/spaceflight-simulator/network-objects/NetworkSettings.cs
+++ src/spaceflight-simulator/network-objects/NetworkSettings.cs
@@ -8,6 +8,8 @@ namespace spaceflight_simulator.network_objects
     {
         public const int SERVER_PORT = 2048;
         public const int DEFAULT_CLIENT_PORT = 2049;
+        public const int SERVER_TCP_PORT = 2050;
+        public const int DEFAULT_CLIENT_TCP_PORT = 2051;
         public const int PACKET_BYTES = 1024;
     }
 }
